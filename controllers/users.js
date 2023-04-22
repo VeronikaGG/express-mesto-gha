b3324d2d8357dc3b81738411b40b65f7867d4c9a
Veronika G <nejnayanika@mail.ru>
2023-04-22 21:09:13 +0300

    Mesto_GHA

--- controllers/users.js
+++ controllers/users.js
@@ -1,10 +1,12 @@
 const User = require('../models/user');
+
 // возвращение всех пользователей
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
 // возвращение пользователя по id
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
@@ -23,18 +25,30 @@ module.exports.getUserById = (req, res) => {
       return res.status(500).send({ message: err.message });
     });
 };
+
 // создание пользователя
+// eslint-disable-next-line consistent-return
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
+  if (!name || !about || !avatar) {
+    return res.status(400).send({ message: 'Не заполнены обязательные поля' });
+  }
 
   User.create({ name, about, avatar })
     .then((user) => res.status(201).send(user))
     .catch((err) => res.status(400).send({ message: err.message }));
 };
+
 // обновление профиля пользователя
+// eslint-disable-next-line consistent-return
 module.exports.updateUserProfile = (req, res) => {
   const { name, about } = req.body;
   const userId = req.user._id;
+
+  if (!name || !about) {
+    return res.status(400).send({ message: 'Не заполнены обязательные поля' });
+  }
+
   User.findByIdAndUpdate(
     userId,
     { name, about },
@@ -52,9 +66,15 @@ module.exports.updateUserProfile = (req, res) => {
 };
 
 // обновление аватара пользователя
+// eslint-disable-next-line consistent-return
 module.exports.updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
   const userId = req.user._id;
+
+  if (!avatar) {
+    return res.status(400).send({ message: 'Не заполнены обязательные поля' });
+  }
+
   User.findByIdAndUpdate(userId, { avatar }, { new: true, runValidators: true })
     // eslint-disable-next-line consistent-return
     .then((user) => {
