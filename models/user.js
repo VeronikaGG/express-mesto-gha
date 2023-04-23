f82f1566c29e41a0f93675ba9f17f4e6b1c7e36e
Veronika G <nejnayanika@mail.ru>
2023-04-23 21:30:43 +0300

    Mesto_GHA

--- models/user.js
+++ models/user.js
@@ -5,19 +5,19 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" - 2'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30'],
   },
   about: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Поле "about" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "about" - 2'],
+    maxlength: [30, 'Максимальная длина поля "about" - 30'],
   },
   avatar: {
     type: String,
-    required: true,
+    required: [true, 'Поле "avatar" должно быть заполнено'],
   },
 });
 
