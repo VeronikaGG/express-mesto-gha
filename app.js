fbfd3167172e34815a11ce2f056a74401a744220
Veronika G <nejnayanika@mail.ru>
2023-04-24 18:32:25 +0300

    Mesto_GHA

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
-const { NOT_FOUND } = require('./errors/errors');
+
+const NOT_FOUND = 404;
 
 app.use(express.json());
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
