eb64c4dca065e1200d01dd9e2cee93b823ec7f0d
Veronika G <nejnayanika@mail.ru>
2023-05-05 15:59:23 +0300

    Mesto_14

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,5 @@ const NotFoundError = require('../errors/notFoundError');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
-router.use('*', (req, res, next) => {
-  next(new NotFoundError('Страница не найдена'));
-});
+router.use('*', NotFoundError);
 module.exports = router;
