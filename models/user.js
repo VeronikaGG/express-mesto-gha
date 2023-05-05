1fde78ee33818341e0ae2521526153cc5ed41fd1
Veronika G <nejnayanika@mail.ru>
2023-05-05 15:53:53 +0300

    Mesto_14

--- models/user.js
+++ models/user.js
@@ -1,59 +1,68 @@
-const mongoose = require('mongoose');
-const isEmail = require('validator/lib/isEmail');
 const bcrypt = require('bcryptjs');
-const AuthorizationError = require('../errors/authorizationError');
+const { isEmail } = require('validator');
+const mongoose = require('mongoose');
+const UnauthorizedError = require('../errors/authorizationError');
+const { REGEXP } = require('../utils/constants');
 
-const { Schema } = mongoose;
-
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" 2 символа'],
-    maxlength: [30, 'Максимальная длина поля "name" 30 символов'],
-    default: 'Жак-Ив Кусто',
-  },
-  about: {
-    type: String,
-    required: [true, 'Поле "about" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "about" 2 символа'],
-    maxlength: [30, 'Максимальная длина поля "about" 30 символов'],
-    default: 'Исследователь',
-  },
-  avatar: {
-    type: String,
-    required: [true, 'Поле "avatar" должно быть заполнено'],
-    default:
-      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: [true, 'Поле "email" должно быть заполнено'],
-    validate: {
-      validator: (v) => isEmail(v),
-      message: 'Неправильный формат почты',
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      default: 'Жак-Ив Кусто',
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+    about: {
+      type: String,
+      default: 'Исследователь',
+      minlength: [2, 'Минимальная длина поля "about" - 2'],
+      maxlength: [30, 'Максимальная длина поля "about" - 30'],
+    },
+    avatar: {
+      type: String,
+      required: [true, 'Поле "avatar" должно быть заполнено'],
+      validate: {
+        validator: (v) => REGEXP.test(v),
+        message: 'Неправильный формат ссылки',
+      },
+      default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    },
+    email: {
+      type: String,
+      required: [true, 'Поле "email" должно быть заполнено'],
+      unique: true,
+      validate: {
+        validator: (v) => isEmail(v),
+        message: 'Неправильный формат почты',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Поле "password" должно быть заполнено'],
+      select: false,
     },
   },
-  password: {
-    type: String,
-    required: [true, 'Поле "password" должно быть заполнено'],
-    select: false,
+  {
+    toObject: {
+      useProjection: true,
+    },
   },
-});
+);
+
 userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
   return this.findOne({ email })
     .select('+password')
     .then((user) => {
       if (!user) {
-        throw new AuthorizationError('Неправильные почта или пароль');
+        throw new UnauthorizedError('Неправильные почта или пароль');
       }
       return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
-          throw new AuthorizationError('Неправильные почта или пароль');
+          throw new UnauthorizedError('Неправильные почта или пароль');
         }
         return user;
       });
     });
 };
+
 module.exports = mongoose.model('user', userSchema);
