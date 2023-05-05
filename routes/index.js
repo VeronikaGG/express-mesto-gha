27f944eb63015373f17273e77a82459edc2e798e
Veronika G <nejnayanika@mail.ru>
2023-05-05 15:22:55 +0300

    Mesto_14

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,9 @@ const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 const NotFoundError = require('../errors/notFoundError');
-const auth = require('../middlewares/auth');
 
-router.use('/users', auth, usersRouter);
-router.use('/cards', auth, cardsRouter);
+router.use('/users', usersRouter);
+router.use('/cards', cardsRouter);
 router.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
