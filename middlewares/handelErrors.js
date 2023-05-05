46ad54486fcb91f54ec58fb77a3729fc329f62eb
Veronika G <nejnayanika@mail.ru>
2023-05-05 19:40:17 +0300

    Mesto_14

--- middlewares/handelErrors.js
+++ middlewares/handelErrors.js
@@ -9,27 +9,39 @@ const ForbiddenError = require('../errors/forbiddenError');
 const UnauthorizedError = require('../errors/authorizationError');
 
 module.exports = ((err, req, res, next) => {
-  if (err instanceof CastError || err instanceof ValidationError) {
-    return res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+  if (err instanceof ValidationError) {
+    const errorMessage = Object.values(err.errors).map((error) => error.message).join(' ');
+    return res.status(BAD_REQUEST).send({
+      message: `Переданы некорректные данные. ${errorMessage}`,
+    });
   }
-
-  if (err instanceof NotFoundError) {
-    return res.status(err.statusCode).send({ message: err.message });
+  if (err instanceof CastError) {
+    return res.status(BAD_REQUEST).send({
+      message: `Передан некорректный ID: ${err.value}`,
+    });
   }
-
-  if (err instanceof ForbiddenError) {
-    return res.status(err.statusCode).send({ message: err.message });
-  }
-
   if (err instanceof UnauthorizedError) {
-    return res.status(err.statusCode).send({ message: err.message });
+    return res.status(err.statusCode).send({
+      message: err.message,
+    });
   }
-
+  if (err instanceof ForbiddenError) {
+    return res.status(err.statusCode).send({
+      message: err.message,
+    });
+  }
+  if (err instanceof NotFoundError) {
+    return res.status(err.statusCode).send({
+      message: err.message,
+    });
+  }
   if (err.code === 11000) {
-    return res.status(CONFLICT_ERROR).send({ message: 'Пользователь с таким email уже существует' });
+    return res.status(CONFLICT_ERROR).send({
+      message: 'Пользователь с таким почтовым адресом уже зарегистрирован',
+    });
   }
-
-  res.status(ERROR_CODE).send({ message: 'На сервере произошла ошибка' });
-
+  res.status(ERROR_CODE).send({
+    message: `Произошла неизвестная ошибка ${err.name}: ${err.message}`,
+  });
   return next();
 });
