ff86feae62fd88e7c680b66c78070fcd664205f4
Veronika G <nejnayanika@mail.ru>
2023-05-05 16:25:48 +0300

    Mesto_14

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 const NotFoundError = require('../errors/notFoundError');
+const auth = require('../middlewares/auth');
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
+router.use('/users', auth, usersRouter);
+router.use('/cards', auth, cardsRouter);
 router.use('*', NotFoundError);
 module.exports = router;
