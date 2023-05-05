6fc50df49d58d028da827c54efcad30c965c7149
Veronika G <nejnayanika@mail.ru>
2023-05-05 21:35:48 +0300

    Mesto

--- app.js
+++ app.js
@@ -5,17 +5,13 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const bodyParser = require('body-parser');
+
 const indexRouter = require('./routes/index');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const handelErrors = require('./middlewares/handelErrors');
 const { usersValidation, loginValidation } = require('./middlewares/requestValidation');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 app.use(indexRouter);
 app.post('/signin', loginValidation, login);
 app.post('/signup', usersValidation, createUser);
--- middlewares/requestValidation.js
+++ middlewares/requestValidation.js
@@ -2,49 +2,49 @@ const { celebrate, Joi } = require('celebrate');
 const { REGEXP } = require('../utils/constants');
 
 const usersValidation = celebrate({
-  body: Joi.object().keys({
+  body: Joi.object({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(REGEXP),
-    email: Joi.string().required().email(),
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
 });
 
 const profileValidation = celebrate({
-  body: Joi.object().keys({
+  body: Joi.object({
     name: Joi.string().min(2).max(30).required(),
     about: Joi.string().min(2).max(30).required(),
   }),
 });
 
 const userValidation = celebrate({
-  params: Joi.object().keys({
+  params: Joi.object({
     userId: Joi.string().hex().length(24).required(),
   }),
 });
 
 const cardValidation = celebrate({
-  params: Joi.object().keys({
+  params: Joi.object({
     cardId: Joi.string().hex().length(24).required(),
   }),
 });
 
 const avatarValidation = celebrate({
-  body: Joi.object().keys({
+  body: Joi.object({
     avatar: Joi.string().pattern(REGEXP).required(),
   }),
 });
 
 const loginValidation = celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
+  body: Joi.object({
+    email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
 });
 
 const cardsValidation = celebrate({
-  body: Joi.object().keys({
+  body: Joi.object({
     name: Joi.string().min(2).max(30).required(),
     link: Joi.string().required().pattern(REGEXP),
   }),
