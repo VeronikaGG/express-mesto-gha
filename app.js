236eff3e45dad8b9adc6c144a12d734a281db306
Veronika G <nejnayanika@mail.ru>
2023-05-05 20:06:28 +0300

    Mesto

--- app.js
+++ app.js
@@ -5,23 +5,25 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const handelErrors = require('./middlewares/handelErrors');
 const { usersValidation, loginValidation } = require('./middlewares/requestValidation');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-});
-
-app.use('/', indexRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(indexRouter);
 app.post('/signin', loginValidation, login);
 app.post('/signup', usersValidation, createUser);
 app.use(cookieParser());
 app.use(auth);
 app.use(handelErrors);
 app.use(errors());
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+});
 app.listen(PORT);
--- middlewares/handelErrors.js
+++ middlewares/handelErrors.js
@@ -1,47 +1,44 @@
-const { CastError, ValidationError } = require('mongoose').Error;
-const {
-  BAD_REQUEST,
-  CONFLICT_ERROR,
-  ERROR_CODE,
-} = require('../utils/constants');
-const NotFoundError = require('../errors/notFoundError');
-const ForbiddenError = require('../errors/forbiddenError');
-const UnauthorizedError = require('../errors/authorizationError');
+// const { CastError, ValidationError } = require('mongoose').Error;
+// const {
+//   BAD_REQUEST,
+//   CONFLICT_ERROR,
+//   ERROR_CODE,
+// } = require('../utils/constants');
+// const NotFoundError = require('../errors/notFoundError');
+// const ForbiddenError = require('../errors/forbiddenError');
+// const UnauthorizedError = require('../errors/authorizationError');
 
-module.exports = ((err, req, res, next) => {
-  if (err instanceof ValidationError) {
-    const errorMessage = Object.values(err.errors).map((error) => error.message).join(' ');
-    return res.status(BAD_REQUEST).send({
-      message: `Переданы некорректные данные. ${errorMessage}`,
-    });
-  }
-  if (err instanceof CastError) {
-    return res.status(BAD_REQUEST).send({
-      message: `Передан некорректный ID: ${err.value}`,
-    });
-  }
-  if (err instanceof UnauthorizedError) {
-    return res.status(err.statusCode).send({
-      message: err.message,
-    });
-  }
-  if (err instanceof ForbiddenError) {
-    return res.status(err.statusCode).send({
-      message: err.message,
-    });
-  }
-  if (err instanceof NotFoundError) {
-    return res.status(err.statusCode).send({
-      message: err.message,
-    });
-  }
-  if (err.code === 11000) {
-    return res.status(CONFLICT_ERROR).send({
-      message: 'Пользователь с таким почтовым адресом уже зарегистрирован',
-    });
-  }
-  res.status(ERROR_CODE).send({
-    message: `Произошла неизвестная ошибка ${err.name}: ${err.message}`,
+module.exports = (err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500
+      ? 'На сервере произошла ошибка'
+      : message,
   });
-  return next();
-});
+  next();
+};
+
+//   if (err instanceof CastError || err instanceof ValidationError) {
+//     return res.status(BAD_REQUEST).send({ message: 'Переданы некорректные данные' });
+//   }
+
+//   if (err instanceof NotFoundError) {
+//     return res.status(err.statusCode).send({ message: err.message });
+//   }
+
+//   if (err instanceof ForbiddenError) {
+//     return res.status(err.statusCode).send({ message: err.message });
+//   }
+
+//   if (err instanceof UnauthorizedError) {
+//     return res.status(err.statusCode).send({ message: err.message });
+//   }
+
+//   if (err.code === 11000) {
+// return res.status(CONFLICT_ERROR).send({ message: 'Пользователь с таким email уже существует' });
+//   }
+
+//   res.status(ERROR_CODE).send({ message: 'На сервере произошла ошибка' });
+
+//   return next();
+// });
