c7007209c87b137182475154bb6b750314483ec6
Veronika G <nejnayanika@mail.ru>
2023-05-24 21:19:41 +0300

    test

--- app.js
+++ app.js
@@ -6,20 +6,38 @@ const { PORT = 3000 } = process.env;
 const app = express();
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { loginValidation, userValidation } = require('./middlewares/requestValidation');
 const handelErrors = require('./middlewares/handelErrors');
 const indexRouter = require('./routes/index');
-const cors = require('./middlewares/cors');
+// const cors = require('./middlewares/cors');
+
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = [
+  'http://localhost:3000',
+  'https://veronikagg.student.nomoredomains.monster',
+  'https://api.veronikagg.student.nomoredomains.monster',
+];
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors);
+app.use(cors({
+  origin: allowedCors,
+  credentials: true,
+}));
+
 app.use(requestLogger);
 
 app.get('/crash-test', () => {
--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,29 +1,28 @@
-const allowedCors = [
-  'https://veronikagg.student.nomoredomains.monster',
-  'https://api.veronikagg.student.nomoredomains.monster',
-  'https://localhost:3000',
-  'http://localhost:3000',
-];
+// const allowedCors = [
+//   'http://localhost:3000',
+//   'https://veronikagg.student.nomoredomains.monster',
+//   'https://api.veronikagg.student.nomoredomains.monster',
+// ];
 
-const cors = (req, res, next) => {
-  const { origin } = req.headers;
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// const cors = (req, res, next) => {
+//   const { origin } = req.headers;
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
 
-    return res.end();
-  }
+//     return res.end();
+//   }
 
-  return next();
-};
+//   return next();
+// };
 
-module.exports = cors;
+// module.exports = cors;
