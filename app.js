7bc380156fa51ffefabbd2f172f85f8920bbb244
Veronika G <nejnayanika@mail.ru>
2023-05-26 14:08:02 +0300

    del_cookies

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
@@ -31,10 +31,10 @@ app.get('/crash-test', () => {
 
 app.post('/signin', loginValidation, login);
 app.post('/signup', userValidation, createUser);
-app.use(cookieParser());
-app.get('/signout', (req, res) => {
-  res.clearCookie('jwt').send({ message: 'Выход' });
-});
+// app.use(cookieParser());
+// app.get('/signout', (req, res) => {
+//   res.clearCookie('jwt').send({ message: 'Выход' });
+// });
 app.use(errorLogger);
 app.use(auth);
 app.use('/', indexRouter);
--- controllers/users.js
+++ controllers/users.js
@@ -5,26 +5,33 @@ const { OK_CODE } = require('../utils/constants');
 const NotFoundError = require('../errors/notFoundError');
 const BadRequestError = require('../errors/badRequestError');
 const ConflictError = require('../errors/conflictError');
+const AuthorizationError = require('../errors/authorizationError');
 const { NODE_ENV, JWT_SECRET } = require('../utils/constants');
 
 // аутентификация
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-
-  User.findUserByCredentials(email, password)
+  User.findOne({ email })
+    .select('+password')
     .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
-        {
+      if (!user) {
+        throw new AuthorizationError('Некорректные email или пароль');
+      }
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          throw new AuthorizationError('Некорректные email или пароль');
+        }
+        const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', {
           expiresIn: '7d',
-        },
-      );
-      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true })
-        .send({ message: 'Аутентификация успешна!' });
+        });
+        res.send({ token });
+      });
     })
-    .catch(next);
+    .catch((err) => {
+      next(err);
+    });
 };
+
 // возвращение всех пользователей
 module.exports.getUsers = (req, res, next) => {
   User.find({})
@@ -107,12 +114,12 @@ module.exports.updateUserAvatar = (req, res, next) => {
   const data = req.body;
   handleUserUpdate(req, res, data, next);
 };
-module.exports.signOut = (req, res, next) => {
-  res
-    .clearCookie('jwt', {
-      maxAge: 3600000 * 24 * 7,
-      httpOnly: true,
-    })
-    .send({ message: 'Вы вышли из профиля ' })
-    .catch(next);
-};
+// module.exports.signOut = (req, res, next) => {
+//   res
+//     .clearCookie('jwt', {
+//       maxAge: 3600000 * 24 * 7,
+//       httpOnly: true,
+//     })
+//     .send({ message: 'Вы вышли из профиля ' })
+//     .catch(next);
+// };
