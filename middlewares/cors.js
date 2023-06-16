35a793589f1add0fb3f0e7b485ce6027df142f73
Veronika G <nejnayanika@mail.ru>
2023-06-16 12:20:13 +0300

    Изменения в корс

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,9 @@
 const allowedCors = [
+  'http://localhost:3001',
+  'https://localhost:3001',
+  '//localhost:3001',
   'localhost:3000',
   'http://localhost:3000',
-  'http://localhost:3001',
-  'localhost:3001',
   'https://veronikagg.student.nomoredomains.monster',
   'https://api.veronikagg.student.nomoredomains.monster',
   'http://api.veronikagg.student.nomoredomains.monster',
@@ -14,17 +15,18 @@ const allowedCors = [
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
+
     return res.end();
   }
 
